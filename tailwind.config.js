42e92f54666a88412b91d616602f94031a8d1e81
CaptchaAC <smgj@mail.ustc.edu.cn>
2023-12-05 00:39:40 +0800

    init the repo

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily:{
+      'heiti': ['黑体', 'SimHei', 'STHeiti', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
